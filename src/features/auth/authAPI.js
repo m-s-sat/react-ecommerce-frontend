39278b55bf7945b3eba1cddc88300fb265c4e1e4
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-06-11 15:15:38 +0530

    Initiated integration with backend

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,7 +1,7 @@
 // A mock function to mimic making an async request for data
 export function createUsers(userData) {
   return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080/users", {
+    const response = await fetch("http://localhost:8080/auth/signup", {
       method: "POST",
       body: JSON.stringify(userData),
       headers: { "content-type": "application/json" },
@@ -14,21 +14,14 @@ export function createUsers(userData) {
 
 export function checkUser(loginInfo){
   return new Promise (async (resolve,reject)=>{
-    const email = loginInfo.email;
-    const password = loginInfo.password;
-    const response = await fetch("http://localhost:8080/users?email="+email);
+    const response = await fetch("http://localhost:8080/auth/login",{
+      method:"POST",
+      body:JSON.stringify(loginInfo),
+      headers:{"content-type":"application/json"},
+    });
     const data = await response.json();
-    if(data.length){
-      if(password===data[0].password){
-        resolve({data:data[0]});
-      }
-      else{
-        reject({message:"wrong credentials"});
-      }
-    }
-    else{
-      reject({message: 'user not found'});
-    }
+    if(response.ok) resolve({data});
+    else reject({message:"Email or password may be wrong"});
   })
 }
 
